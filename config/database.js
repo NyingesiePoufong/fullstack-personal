ccedea98c5b2972ab3d083c4e0a912babe483a07
NyingesiePoufong <nyingesie.poufong.dev@gmail.com>
2019-11-17 20:23:30 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://admin:admin@loginiter1-ge65u.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'fullstack-login'
+};
